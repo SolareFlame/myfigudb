99dc36fcc9d401b5e651df586dd7b750dd638d57
solare <zeldac332@gmail.com>
2025-06-01 03:49:09 +0200

    🚧 DatabaseSeeder for default seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Figure;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(StatusesTableSeeder::class);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin@myfigudb.fr',
+            'password' => bcrypt('habibiroot'),
         ]);
+
+        Figure::factory()->count(10)->create();
     }
 }
