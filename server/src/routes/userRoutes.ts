c151ffdbf417249344819b6421fb83d4ddd7c262
SolareFlame <zeldac332@gmail.com>
2025-06-10 00:13:23 +0200

    ✨ Refactor user management: remove unused user controller methods and update user service for handle-based user creation

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,18 +1,11 @@
 import express, {RequestHandler} from 'express'
 import {
-    getUsers,
     getUserById,
-    createUser,
-    updateUser,
-    deleteUser
 } from '../controllers/userController'
 
 const router = express.Router()
 
-router.get('/', getUsers as RequestHandler)
+//router.get('/', getUsers as RequestHandler)
 router.get('/:id', getUserById as RequestHandler)
-router.post('/', createUser as RequestHandler)
-router.patch('/:id', updateUser as RequestHandler)
-router.delete('/:id', deleteUser as RequestHandler)
 
 export default router
