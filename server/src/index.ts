be40470fb1728dd975f1528233ee0f18254390c9
SolareFlame <zeldac332@gmail.com>
2025-06-09 15:40:45 +0200

    🚀 Set up Express server with routes for figures and users

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import figureRoutes from "./routes/figureRoutes";
+import userRoutes from "./routes/userRoutes";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/figures', figureRoutes)
+app.use('/api/users', userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
