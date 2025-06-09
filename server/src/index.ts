1065f59d9e3a3eb20d63219fa9a8f50c47b4c05b
SolareFlame <zeldac332@gmail.com>
2025-06-10 00:13:14 +0200

    ✨ Add image upload routes and JWT_SECRET validation in the server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,17 @@ import dotenv from 'dotenv';
 
 import figureRoutes from "./routes/figureRoutes";
 import userRoutes from "./routes/userRoutes";
+import authRoutes from "./routes/authRoutes";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+if (!process.env.JWT_SECRET) {
+    throw new Error('JWT_SECRET is not set in environment variables');
+}
+
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +22,10 @@ app.use(express.json());
 // Routes
 app.use('/api/figures', figureRoutes)
 app.use('/api/users', userRoutes);
+app.use('/api/auth/', authRoutes);
+
+app.use('/uploads', express.static('uploads'));
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
